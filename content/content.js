a1c0df406aeb7d28592d3643be66fdcf935ab434
Xavier Maruff <62425409+Xavier-Maruff@users.noreply.github.com>
2021-04-15 23:22:24 +1000

    intial

---
+++ content/content.js
@@ -0,0 +1,35 @@
+
+const random = max => {
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + 1));
+}
+
+const oldManOnclick = _=> {
+    alert("you found the secret old man. Now try it in real life!");
+    deleteOldMan();
+    createOldMan();
+    return;
+}
+
+const deleteOldMan = _=> {
+    const oldManNode = document.getElementById("oldman");
+    oldManNode.remove();
+
+}
+
+const createOldMan = _=> {
+    const nodes = document.querySelectorAll('*');
+    const oldManNode = nodes[random(nodes.length)];
+
+    const oldManImg = document.createElement("img");
+    oldManImg.src = chrome.extension.getURL("/icons/icon.png");
+    oldManImg.width = 20;
+    oldManImg.id = "oldman";
+    oldManImg.addEventListener('click', oldManOnclick);
+
+    oldManNode.appendChild(oldManImg);
+
+    return;
+}
+
+createOldMan();
